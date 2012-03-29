7c359b7bfdef61a12764c7bf189a5b7a804748db
amoneus <fgkft8@yahoo.co.jp>
2012-03-29 12:37:42 +0900

    first commit

---
+++ rabbit.js
@@ -0,0 +1,18 @@
+﻿var rabbit = 1;
+var i = 0;
+var pair;
+
+var f = function(x){
+	pair = rabbit / 2;
+	console.log("month = " + i + " num = " + rabbit + " pair = " + pair);
+	rabbit += i;
+	i++;
+
+	if(x >= i){
+		f(x);
+	}
+	
+	return true;
+}
+
+f(12);
